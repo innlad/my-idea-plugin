b8ab6ccc4ac91d23caba3a992754c98c0bac283b
GitHub Action <action@github.com>
2021-07-16 02:29:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/innlad/myideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/innlad/myideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.innlad.myideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.innlad.myideaplugin.MyBundle
 
 class MyApplicationService {
 
